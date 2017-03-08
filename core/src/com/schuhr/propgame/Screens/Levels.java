00c53199d5b1ec3a49dcc8da30240a14be3fbaab
schuhbacca <schuh.m.ryan@gmail.com>
2017-03-08 15:12:42 -0500

    Added level two and Tileset. Updated world.step to make game more fluid.

--- core/src/com/schuhr/propgame/Screens/Levels.java
+++ core/src/com/schuhr/propgame/Screens/Levels.java
@@ -180,7 +180,7 @@ public class Levels implements Screen {
 
     public void update(float dt) {
         handleInput(dt);
-        world.step(1 / 45f, 6, 2);
+        world.step(1 / 35f, 6, 2);
         player.update(dt);
         for (Enemy enemy : creator.getRedEnemies()) {
             enemy.update(dt);
